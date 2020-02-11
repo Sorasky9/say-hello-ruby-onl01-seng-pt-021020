61199a64f02940e6caef33704427e742d9ccae05
Malcom Fetus <sora.vista@gmail.com>
2020-02-11 16:16:02 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -2,3 +2,7 @@
 def say_hello(name="Kent Beck", language="Ruby Programmer")
   puts "Hello #{name}!"
 end 
+
+def say_hello(name="Kent Beck", language="Ruby Programmer")
+  puts "Hello #{language}!"
+end
